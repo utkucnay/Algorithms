a529a3ffba494feabaa111299273167e55f09a9a
utkucnay <utkucnay@gmailcom>
2023-07-01 18:30:08 -0400

    Solved AdventOfCode 2015 day1 part 2

--- AdventOfCode/2015/day1/cpp/main.cpp
+++ AdventOfCode/2015/day1/cpp/main.cpp
@@ -12,12 +12,26 @@ int main()
     inputfile >> input;
     inputfile.close();
 
-    long long sum = 0;
-
-    for(char ch : input)
     {
-        sum += 1 * (ch == '(') + -1 * (ch == ')');
+        //part one
+        long long sum = 0;
+        for(char ch : input)
+        {
+            sum += 1 * (ch == '(') + -1 * (ch == ')');
+        }
+        std::cout << std::to_string(sum) << std::endl;
     }
-
-    std::cout << std::to_string(sum) << std::endl;
+    {
+        //part two
+        long long sum = 0;
+        long long firstBasementPos = -1;
+        for(long long i = 0; i < input.size(); i++)
+        {
+            sum += 1 * (input.at(i) == '(') + -1 * (input.at(i) == ')');
+            firstBasementPos += (i + 2) *
+                                (sum == -1) *
+                                (firstBasementPos == -1);
+        }
+        std::cout << std::to_string(firstBasementPos) << std::endl;
+    }
 }
